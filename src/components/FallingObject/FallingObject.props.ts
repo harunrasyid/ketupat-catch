d55c77dce94e8397f3eb582dde5515ca9046ebb4
harunar21 <harun@rekosistem.com>
2025-03-30 11:19:19 +0700

    Initial commit

--- src/components/FallingObject/FallingObject.props.ts
+++ src/components/FallingObject/FallingObject.props.ts
@@ -3,7 +3,7 @@ import { RefObject } from "react";
 export interface IFallingObjectProps {
   url: string;
   position: [number, number, number];
-  onCatch: (id: number, bad: boolean) => void;
+  onCatch: (bad: boolean) => void;
   onMiss: () => void;
   bad: boolean;
   id: number;
